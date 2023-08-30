a4bf84e1e712f22770e566cf2b6735efd31a6a99
Nicole9410 <140712581+Nicole9410@users.noreply.github.com>
2023-08-30 13:19:03 +0200

    Added GitHub repo; footer
    Footer has link to code

--- src/App.js
+++ src/App.js
@@ -3,7 +3,10 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      
+      <footer>
+        <a href="https://github.com/Nicole9410/react-weather-search-engine" target="_blank" rel="noreferrer"> Open-source code </a>
+        by Nicole de Wee.
+      </footer>
     </div>
   );
 }
