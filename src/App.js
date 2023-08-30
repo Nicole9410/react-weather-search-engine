ae3e91a4e0804adf607afc70e39831c90e5dd176
Nicole9410 <140712581+Nicole9410@users.noreply.github.com>
2023-08-30 15:27:06 +0200

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        <a href="https://github.com/Nicole9410/react-weather-search-engine" target="_blank" rel="noreferrer"> Open-source code </a>
-        by Nicole de Wee.
-      </footer>
+      <div className="container">
+        <footer>
+          <a href="https://github.com/Nicole9410/react-weather-search-engine" target="_blank" rel="noreferrer"> Open-source code </a>
+          by Nicole de Wee.
+        </footer>
+      </div>
     </div>
   );
 }
